f59dffc559ff60d3f9570ed2312270799a51e33a
Thinh-Phung <phunghuuthinh01@gmail.com>
2022-12-22 20:47:40 +0700

    Redux Tool kit setup

---
+++ src/store/hooks.ts
@@ -0,0 +1,6 @@
+import { AppDispatch, RootState } from "./index";
+import { useDispatch } from "react-redux";
+import { TypedUseSelectorHook } from "react-redux/es/types";
+import { useSelector } from "react-redux/es/hooks/useSelector";
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
---
+++ src/store/reducers/getHomePageVideos.ts
@@ -0,0 +1,19 @@
+import { YOUTUBE_API_URL } from "./../../ultils/constants";
+import { RootState } from "./../index";
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+const API_KEY = process.env.REACT_APP_YOUTUBE_DATA_API_KEY;
+export const getHomePageVideos = createAsyncThunk(
+  "youtubeAPP/homePageVidoes",
+  async (isNext: boolean, { getState }) => {
+    const {
+      youtubeApp: { nextPageToken: nextPageTokenFromState, videos },
+    } = getState() as RootState;
+    const {
+      data: { items, nextPageToken },
+    } = await axios.get(`${YOUTUBE_API_URL}/
+    search?maxResults=20&q="reactjs projects"&key=${API_KEY}&part=
+    snippet&type=video`);
+    console.log(items);
+  }
+);
